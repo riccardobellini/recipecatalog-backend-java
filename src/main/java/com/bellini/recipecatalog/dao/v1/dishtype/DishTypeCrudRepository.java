b50d0d8342fa7883ccb220ce6451f0c34368eadc
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2018-07-15 13:24:12 +0200

    Use paging repository

--- src/main/java/com/bellini/recipecatalog/dao/v1/dishtype/DishTypeCrudRepository.java
+++ src/main/java/com/bellini/recipecatalog/dao/v1/dishtype/DishTypeCrudRepository.java
@@ -2,11 +2,11 @@ package com.bellini.recipecatalog.dao.v1.dishtype;
 
 import java.util.Collection;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.bellini.recipecatalog.model.v1.DishType;
 
-public interface DishTypeCrudRepository extends CrudRepository<DishType, Long> {
+public interface DishTypeCrudRepository extends PagingAndSortingRepository<DishType, Long> {
 
 //    Collection<DishType> findAll();
     Collection<DishType> findByNameIgnoreCase(String name);
