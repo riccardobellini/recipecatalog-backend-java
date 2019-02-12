0717f99f2073a0390acc0b5d1e3c1f0414228d35
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2019-02-12 11:52:13 +0100

    Convert ingredient findAll() method to JDBC template

--- src/main/java/com/bellini/recipecatalog/dao/v1/ingredient/IngredientRepository.java
+++ src/main/java/com/bellini/recipecatalog/dao/v1/ingredient/IngredientRepository.java
@@ -1,15 +1,26 @@
 package com.bellini.recipecatalog.dao.v1.ingredient;
 
 import java.util.Collection;
+import java.util.Optional;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.bellini.recipecatalog.model.v1.Ingredient;
 
-public interface IngredientRepository extends PagingAndSortingRepository<Ingredient, Long>, IngredientCustomRepository {
+public interface IngredientRepository {
 
     Collection<Ingredient> findByNameIgnoreCase(String name);
-    
+
     Collection<Ingredient> findByNameIgnoreCaseContaining(String name);
+
+    Page<Ingredient> findByNameIgnoreCaseContaining(String name, Pageable page);
+
+    Page<Ingredient> findAll(Pageable page);
     
+    Ingredient save(Ingredient ingr);
+    
+    Optional<Ingredient> findById(Long id);
+    
+    void deleteById(Long id);
 }
--- src/main/java/com/bellini/recipecatalog/dao/v1/ingredient/IngredientRepositoryImpl.java
+++ src/main/java/com/bellini/recipecatalog/dao/v1/ingredient/IngredientRepositoryImpl.java
@@ -1,37 +1,106 @@
 package com.bellini.recipecatalog.dao.v1.ingredient;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Collection;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import com.bellini.recipecatalog.model.v1.Ingredient;
 
-public class IngredientRepositoryImpl implements IngredientCustomRepository {
-    
-    @Autowired
-    EntityManager emf;
+@Repository
+public class IngredientRepositoryImpl implements IngredientRepository {
 
-    @SuppressWarnings("unchecked")
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Collection<Ingredient> findByNameIgnoreCase(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<Ingredient> findByNameIgnoreCaseContaining(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
     @Override
     public Page<Ingredient> findByNameIgnoreCaseContaining(String name, Pageable page) {
-        
-        Query q = emf.createQuery("SELECT i FROM Ingredient i WHERE i.name LIKE :iName")
-            .setParameter("iName", "%" + name + "%")
-            .setFirstResult((int) page.getOffset())
-            .setMaxResults(page.getPageSize());
-        List<Ingredient> ingrList = q.getResultList();
-        
-        Query qTotal = emf.createQuery("SELECT count(i.id) FROM Ingredient i WHERE i.name LIKE :iName")
-            .setParameter("iName", "%" + name + "%");
-        long total = (long) qTotal.getSingleResult();
-        
-        return new PageImpl<>(ingrList, page, total);
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Page<Ingredient> findAll(Pageable page) {
+        List<Ingredient> result = jdbcTemplate.query(allSelectSQL(), (stmt) -> {
+            stmt.setInt(1, page.getPageNumber() * page.getPageSize());
+            stmt.setInt(2, page.getPageSize());
+        }, defaultMapper());
+        Long count = getRowCount();
+        return new PageImpl<>(result, PageRequest.of(page.getPageNumber(), page.getPageSize()), count);
+    }
+
+    private String allSelectSQL() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT i.ID, i.NAME, i.CREATION_TIME, i.LAST_MODIFICATION_TIME ");
+        sb.append("FROM INGREDIENT i ");
+        sb.append("ORDER BY i.NAME ASC ");
+        sb.append("LIMIT ?, ?");
+        return sb.toString();
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingr) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Ingredient> findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private RowMapper<Ingredient> defaultMapper() {
+        return (rs, row) -> {
+            Ingredient ingr = new Ingredient();
+            ingr.setId(rs.getLong("ID"));
+            ingr.setName(rs.getString("NAME"));
+            Timestamp creationTimestamp = rs.getTimestamp("CREATION_TIME");
+            if (creationTimestamp != null) {
+                ingr.setCreationTime(Instant.ofEpochMilli(creationTimestamp.getTime()));
+            }
+            Timestamp lastModificationTimestamp = rs.getTimestamp("LAST_MODIFICATION_TIME");
+            if (lastModificationTimestamp != null) {
+                ingr.setLastModificationTime(Instant.ofEpochMilli(lastModificationTimestamp.getTime()));
+            }
+            return ingr;
+        };
+    }
+
+    private Long getRowCount() {
+        return jdbcTemplate.queryForObject(countSQL(), Long.class);
+    }
+
+    private String countSQL() {
+        return "SELECT COUNT(*) AS total FROM INGREDIENT";
     }
 
 }
