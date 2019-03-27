5643b36e980524f1e77c2d2091d710349d04e420
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2019-03-27 10:26:38 +0100

    Tests on findByNameIgnoreCaseContaining dish type

--- src/test/java/com/bellini/recipecatalog/test/repository/dishtype/DishTypeRepositoryTest.java
+++ src/test/java/com/bellini/recipecatalog/test/repository/dishtype/DishTypeRepositoryTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,8 +53,7 @@ public class DishTypeRepositoryTest {
 
     @Test
     public void findAll_shouldHandlePaginationCorrectly() {
-        final List<DishType> content = repo.findAll(PageRequest.of(0, Integer.MAX_VALUE)).getContent(); // retrieve all
-                                                                                                        // elements
+        final List<DishType> content = repo.findAll(PageRequest.of(0, Integer.MAX_VALUE)).getContent(); // retrieve all elements
         // fetch two items each time until no results are available
         int startFrom = 0, fetch = 2, total = 0;
         List<DishType> contentToCheck = new ArrayList<>();
@@ -87,6 +87,37 @@ public class DishTypeRepositoryTest {
 
         assertThat(result, notNullValue());
         assertThat(result, empty());
+    }
+
+    @Test
+    public void findByNameIgnoreCaseContaining_shouldReturnCorrectResults() {
+        Page<DishType> result = repo.findByNameIgnoreCaseContaining("ast", PageRequest.of(0, 10));
+
+        assertThat(result, notNullValue());
+        assertThat(result, allOf(
+            hasItem(Matchers.<DishType>hasProperty("name", is("Roast"))),
+            hasItem(Matchers.<DishType>hasProperty("name", is("Pasta")))));
+    }
+
+    @Test
+    public void findByNameIgnoreCaseContaining_shouldHandlePaginationCorrectly() {
+        final List<DishType> content = repo.findByNameIgnoreCaseContaining("ast", PageRequest.of(0, Integer.MAX_VALUE)).getContent(); // retrieve all elements
+        // fetch two items each time until no results are available
+        int startFrom = 0, fetch = 1, total = 0;
+        List<DishType> contentToCheck = new ArrayList<>();
+        List<DishType> tmpContent = null;
+        do {
+            tmpContent = repo.findByNameIgnoreCaseContaining("ast", PageRequest.of(startFrom, fetch)).getContent();
+            total += tmpContent.size();
+            contentToCheck.addAll(tmpContent);
+            ++startFrom;
+        } while (!tmpContent.isEmpty());
+
+        // check for matching number of elements
+        assertThat(total, comparesEqualTo(2));
+
+        // check that the lists are equal
+        assertThat(contentToCheck, is(content));
     }
 
 }
