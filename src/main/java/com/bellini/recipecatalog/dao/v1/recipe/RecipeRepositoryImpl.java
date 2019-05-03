fdc8da484191e019ce2a3eb267612023ff41e16b
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2019-05-03 15:58:31 +0200

    Move business logic from repository to service

--- src/main/java/com/bellini/recipecatalog/dao/v1/recipe/RecipeRepositoryImpl.java
+++ src/main/java/com/bellini/recipecatalog/dao/v1/recipe/RecipeRepositoryImpl.java
@@ -4,7 +4,6 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,14 +16,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
-import com.bellini.recipecatalog.dao.v1.book.BookRepository;
-import com.bellini.recipecatalog.dao.v1.dishtype.DishTypeRepository;
-import com.bellini.recipecatalog.dao.v1.ingredient.IngredientRepository;
-import com.bellini.recipecatalog.dao.v1.publication.PublicationRepository;
-import com.bellini.recipecatalog.model.v1.Book;
-import com.bellini.recipecatalog.model.v1.DishType;
-import com.bellini.recipecatalog.model.v1.Ingredient;
-import com.bellini.recipecatalog.model.v1.Publication;
 import com.bellini.recipecatalog.model.v1.Recipe;
 
 @Repository
@@ -33,18 +24,6 @@ public class RecipeRepositoryImpl implements RecipeRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private DishTypeRepository dishTypeRepo;
-
-    @Autowired
-    private IngredientRepository ingredientRepo;
-
-    @Autowired
-    private BookRepository bookRepo;
-
-    @Autowired
-    private PublicationRepository pubRepo;
-
     @Override
     public Page<Recipe> findAll(Pageable page) {
         List<Recipe> result = jdbcTemplate.query(allSelectSQL(), (stmt) -> {
@@ -52,18 +31,6 @@ public class RecipeRepositoryImpl implements RecipeRepository {
             stmt.setInt(2, page.getPageSize());
         }, defaultMapper());
         Long count = getRowCount();
-        for (Recipe rec : result) {
-            rec.setDishtypes(dishTypeRepo.findByRecipeId(rec.getId()));
-            rec.setIngredients(ingredientRepo.findByRecipeId(rec.getId()));
-            Optional<Book> optBook = bookRepo.findByRecipeId(rec.getId());
-            if (optBook.isPresent()) {
-                rec.setBook(optBook.get());
-            }
-            Optional<Publication> optPub = pubRepo.findByRecipeId(rec.getId());
-            if (optPub.isPresent()) {
-                rec.setPublication(optPub.get());
-            }
-        }
         return new PageImpl<>(result, page, count);
     }
 
@@ -86,29 +53,9 @@ public class RecipeRepositoryImpl implements RecipeRepository {
 
     @Override
     public Recipe save(Recipe recipe) {
-        // TODO implement transaction
-        // first, store recipe
         Long recipeId = storeRecipe(recipe);
         // store other elements only if recipe id is valid
         if (recipeId != null) {
-            // check list of ingredients
-            Collection<Ingredient> ingredients = recipe.getIngredients();
-            for (Ingredient ingr : ingredients) {
-                ingredientRepo.attachToRecipe(ingr.getId(), recipeId);
-            }
-            Collection<DishType> dishtypes = recipe.getDishtypes();
-            for (DishType dt : dishtypes) {
-                dishTypeRepo.attachToRecipe(dt.getId(), recipeId);
-            }
-            Book book = recipe.getBook();
-            if (book != null) {
-                bookRepo.attachToRecipe(book.getId(), recipeId);
-            }
-            Publication publication = recipe.getPublication();
-            if (publication != null) {
-                pubRepo.attachToRecipe(publication.getId(), recipeId);
-            }
-
             return findById(recipeId).get();
         }
         return null;
@@ -142,21 +89,7 @@ public class RecipeRepositoryImpl implements RecipeRepository {
         List<Recipe> result = jdbcTemplate.query(byIdSelectSQL(), (stmt) -> {
             stmt.setLong(1, id);
         }, defaultMapper());
-        if (!result.isEmpty()) {
-            Recipe rec = result.get(0);
-            rec.setDishtypes(dishTypeRepo.findByRecipeId(rec.getId()));
-            rec.setIngredients(ingredientRepo.findByRecipeId(rec.getId()));
-            Optional<Book> optBook = bookRepo.findByRecipeId(rec.getId());
-            if (optBook.isPresent()) {
-                rec.setBook(optBook.get());
-            }
-            Optional<Publication> optPub = pubRepo.findByRecipeId(rec.getId());
-            if (optPub.isPresent()) {
-                rec.setPublication(optPub.get());
-            }
-            return Optional.of(rec);
-        }
-        return Optional.empty();
+        return !result.isEmpty() ? Optional.of(result.get(0)) : Optional.empty();
     }
 
     private String byIdSelectSQL() {
--- src/main/java/com/bellini/recipecatalog/service/v1/recipe/RecipeServiceImpl.java
+++ src/main/java/com/bellini/recipecatalog/service/v1/recipe/RecipeServiceImpl.java
@@ -1,15 +1,23 @@
 package com.bellini.recipecatalog.service.v1.recipe;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.bellini.recipecatalog.dao.v1.book.BookRepository;
+import com.bellini.recipecatalog.dao.v1.dishtype.DishTypeRepository;
+import com.bellini.recipecatalog.dao.v1.ingredient.IngredientRepository;
+import com.bellini.recipecatalog.dao.v1.publication.PublicationRepository;
 import com.bellini.recipecatalog.dao.v1.recipe.RecipeRepository;
 import com.bellini.recipecatalog.exception.ingredient.NotExistingIngredientException;
+import com.bellini.recipecatalog.model.v1.Book;
+import com.bellini.recipecatalog.model.v1.DishType;
+import com.bellini.recipecatalog.model.v1.Ingredient;
+import com.bellini.recipecatalog.model.v1.Publication;
 import com.bellini.recipecatalog.model.v1.Recipe;
 
 @Service
@@ -18,25 +26,78 @@ public class RecipeServiceImpl implements RecipeService {
     @Autowired
     private RecipeRepository repo;
 
+    @Autowired
+    private DishTypeRepository dishTypeRepo;
+
+    @Autowired
+    private IngredientRepository ingredientRepo;
+
+    @Autowired
+    private BookRepository bookRepo;
+
+    @Autowired
+    private PublicationRepository pubRepo;
+
     @Override
-    public Recipe create(Recipe pub) {
+    public Recipe create(Recipe rec) {
         // TODO performs check
-        return repo.save(pub);
+        final Recipe saved = repo.save(rec);
+        final Long recipeId = saved.getId();
+        Collection<Ingredient> ingredients = saved.getIngredients();
+        for (Ingredient ingr : ingredients) {
+            ingredientRepo.attachToRecipe(ingr.getId(), recipeId);
+        }
+        Collection<DishType> dishtypes = saved.getDishtypes();
+        for (DishType dt : dishtypes) {
+            dishTypeRepo.attachToRecipe(dt.getId(), recipeId);
+        }
+        Book book = saved.getBook();
+        if (book != null) {
+            bookRepo.attachToRecipe(book.getId(), recipeId);
+        }
+        Publication publication = saved.getPublication();
+        if (publication != null) {
+            pubRepo.attachToRecipe(publication.getId(), recipeId);
+        }
+        return saved;
     }
 
     @Override
     public Recipe get(Long id) {
-        Optional<Recipe> optPub = repo.findById(id);
-        if (!optPub.isPresent()) {
-            throw new NotExistingIngredientException(id);
+        final Optional<Recipe> optRec = repo.findById(id);
+        if (!optRec.isPresent()) {
+            throw new NotExistingIngredientException(id); // FIXME change exception
         }
-        return optPub.get();
+        final Recipe rec = optRec.get();
+        rec.setDishtypes(dishTypeRepo.findByRecipeId(rec.getId()));
+        rec.setIngredients(ingredientRepo.findByRecipeId(rec.getId()));
+        Optional<Book> optBook = bookRepo.findByRecipeId(rec.getId());
+        if (optBook.isPresent()) {
+            rec.setBook(optBook.get());
+        }
+        Optional<Publication> optPub = pubRepo.findByRecipeId(rec.getId());
+        if (optPub.isPresent()) {
+            rec.setPublication(optPub.get());
+        }
+        return optRec.get();
     }
 
     @Override
-    @Transactional
     public Page<Recipe> getAll(Pageable page) {
-        return repo.findAll(page);
+        final Page<Recipe> result = repo.findAll(page);
+        for (Recipe rec : result) {
+            rec.setDishtypes(dishTypeRepo.findByRecipeId(rec.getId()));
+            rec.setIngredients(ingredientRepo.findByRecipeId(rec.getId()));
+            Optional<Book> optBook = bookRepo.findByRecipeId(rec.getId());
+            if (optBook.isPresent()) {
+                rec.setBook(optBook.get());
+            }
+            Optional<Publication> optPub = pubRepo.findByRecipeId(rec.getId());
+            if (optPub.isPresent()) {
+                rec.setPublication(optPub.get());
+            }
+        }
+        return result;
     }
 
 }
