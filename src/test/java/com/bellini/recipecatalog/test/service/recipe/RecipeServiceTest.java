f04ed375f32fed3b3de8de248238f3d8c3524712
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2019-07-12 10:53:13 +0200

    Add tests for get() in recipe service

--- src/test/java/com/bellini/recipecatalog/test/service/recipe/RecipeServiceTest.java
+++ src/test/java/com/bellini/recipecatalog/test/service/recipe/RecipeServiceTest.java
@@ -7,6 +7,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import java.time.Instant;
+import java.util.Collections;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,6 +22,7 @@ import com.bellini.recipecatalog.dao.v1.dishtype.DishTypeRepository;
 import com.bellini.recipecatalog.dao.v1.ingredient.IngredientRepository;
 import com.bellini.recipecatalog.dao.v1.publication.PublicationRepository;
 import com.bellini.recipecatalog.dao.v1.recipe.RecipeRepository;
+import com.bellini.recipecatalog.exception.recipe.NotExistingRecipeException;
 import com.bellini.recipecatalog.model.v1.Book;
 import com.bellini.recipecatalog.model.v1.DishType;
 import com.bellini.recipecatalog.model.v1.Ingredient;
@@ -133,5 +136,53 @@ public class RecipeServiceTest {
         pub.setVolume(1);
         pub.setYear(2019);
         return pub;
+    }
+
+    @Test(expected = NotExistingRecipeException.class)
+    public void get_shouldThrowExceptionWhenNotFound() {
+        when(recipeRepo.findById(anyLong())).thenReturn(Optional.empty());
+        recipeSrv.get(1L);
+    }
+
+    @Test
+    public void get_shouldCallOtherRepositoriesAndSetEntities() {
+        when(recipeRepo.findById(anyLong())).thenReturn(Optional.of(dummyRecipeSlim()));
+        when(dtRepo.findByRecipeId(anyLong())).thenReturn(Collections.singleton(dummyDishType()));
+        when(ingrRepo.findByRecipeId(anyLong())).thenReturn(Collections.singleton(dummyIngredient(1L, DUMMY_INGREDIENT_1_NAME)));
+        when(bookRepo.findByRecipeId(anyLong())).thenReturn(Optional.of(dummyBook()));
+        when(pubRepo.findByRecipeId(anyLong())).thenReturn(Optional.of(dummyPublication()));
+
+        Recipe theRecipe = recipeSrv.get(1L);
+        assertFalse(theRecipe.getDishtypes().isEmpty());
+        assertFalse(theRecipe.getIngredients().isEmpty());
+        assertThat(theRecipe.getBook(), notNullValue());
+        assertThat(theRecipe.getPublication(), notNullValue());
+
+        verify(recipeRepo).findById(anyLong());
+        verify(dtRepo, atLeastOnce()).findByRecipeId(anyLong());
+        verify(ingrRepo, atLeastOnce()).findByRecipeId(anyLong());
+        verify(bookRepo).findByRecipeId(anyLong());
+        verify(pubRepo).findByRecipeId(anyLong());
+    }
+
+    @Test
+    public void get_shouldCallOtherRepositoriesAndLeaveNullFieldsIfEntitiesAreNotFound() {
+        when(recipeRepo.findById(anyLong())).thenReturn(Optional.of(dummyRecipeSlim()));
+        when(dtRepo.findByRecipeId(anyLong())).thenReturn(Collections.singleton(dummyDishType()));
+        when(ingrRepo.findByRecipeId(anyLong())).thenReturn(Collections.singleton(dummyIngredient(1L, DUMMY_INGREDIENT_1_NAME)));
+        when(bookRepo.findByRecipeId(anyLong())).thenReturn(Optional.empty());
+        when(pubRepo.findByRecipeId(anyLong())).thenReturn(Optional.empty());
+
+        Recipe theRecipe = recipeSrv.get(1L);
+        assertFalse(theRecipe.getDishtypes().isEmpty());
+        assertFalse(theRecipe.getIngredients().isEmpty());
+        assertThat(theRecipe.getBook(), nullValue());
+        assertThat(theRecipe.getPublication(), nullValue());
+
+        verify(recipeRepo).findById(anyLong());
+        verify(dtRepo, atLeastOnce()).findByRecipeId(anyLong());
+        verify(ingrRepo, atLeastOnce()).findByRecipeId(anyLong());
+        verify(bookRepo).findByRecipeId(anyLong());
+        verify(pubRepo).findByRecipeId(anyLong());
     }
 }
